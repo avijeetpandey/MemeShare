91fc806bce88e9bbafaa6064674dccfab77bd994
Avijeet Pandey <avijeetpandey25@gmail.com>
2021-07-22 23:46:47 +0530

    Add : Meme image from API

--- app/src/main/java/com/avijeet/memeshare/MainActivity.kt
+++ app/src/main/java/com/avijeet/memeshare/MainActivity.kt
@@ -4,15 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadMeme()
     }
 
     // function to load meme
@@ -26,8 +27,8 @@ class MainActivity : AppCompatActivity() {
             url,null,
             { response ->
                 val url = response.getString("url")
-
-        },
+                Glide.with(this).load(url).into(memeImage)
+            },
             {  })
 
         // add the request to the queue
