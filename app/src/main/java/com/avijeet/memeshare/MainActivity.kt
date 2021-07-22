140cc83285de34a803be48671a3a0d392e2ac517
Avijeet Pandey <avijeetpandey25@gmail.com>
2021-07-23 00:02:35 +0530

    Add : Complete image load functionality

--- app/src/main/java/com/avijeet/memeshare/MainActivity.kt
+++ app/src/main/java/com/avijeet/memeshare/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.avijeet.memeshare
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,6 +8,10 @@ import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,19 +22,41 @@ class MainActivity : AppCompatActivity() {
     }
 
     // function to load meme
-    private fun loadMeme(){
+    private fun loadMeme() {
+        progressBar.visibility = View.VISIBLE
         val queue = Volley.newRequestQueue(this)
         val url = "https://meme-api.herokuapp.com/gimme"
 
         // Request a string response from the provided url
         val jsonObjectRequest = JsonObjectRequest(
             Request.Method.GET,
-            url,null,
+            url, null,
             { response ->
                 val url = response.getString("url")
-                Glide.with(this).load(url).into(memeImage)
+                Glide.with(this).load(url).listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.visibility = View.GONE
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.visibility = View.GONE
+                        return false
+                    }
+                }).into(memeImage)
             },
-            {  })
+            { })
 
         // add the request to the queue
         queue.add(jsonObjectRequest)
